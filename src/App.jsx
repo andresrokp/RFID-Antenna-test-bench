d8e0d34089e14fc849917a6d26d87b6128ebb41c
andresrokp <afrokap@gmail.com>
2023-08-21 13:32:36 -0500

    chore: second react gpt draft
    Good lists dynamics
    Good sending logic
    Bad asignation algorithm

--- src/App.jsx
+++ src/App.jsx
@@ -1,115 +1,122 @@
-import React, { useState, useEffect } from 'react';
-import './App.css'; // Make sure to have a corresponding CSS file
+import { useState } from 'react';
 
-const initialElements = [
-  { name: 'Element 1', tagID: '1100EE00E2000020401302262550CB697740' },
-  { name: 'Element 2', tagID: '1100EE00E2000020401302232560CACF6939' },
-  { name: 'Element 3', tagID: '1100EE00E2000020401302182510C3D16B04' },
-  // ... other elements
+// Initial data
+const initialStores = [
+  {
+    name: 'Store A',
+    latitude: 7.094250,
+    longitude: -73.858397,
+    BodegaId: 'storeA',
+    tbToken: 'tbTokenA',
+    ready: false,
+    elements: []
+  },
+  {
+    name: 'Store B',
+    latitude: 7.094250,
+    longitude: -73.858397,
+    BodegaId: 'storeB',
+    tbToken: 'tbTokenB',
+    ready: false,
+    elements: []
+  },
+  // Add more stores here...
 ];
 
-const stores = [
-  { name: 'Store A', latitude: 7.094250, longitude: -73.858397, bodegaID: 'storeA', tbToken: 'tokenA' },
-  { name: 'Store B', latitude: 7.123456, longitude: -73.987654, bodegaID: 'storeB', tbToken: 'tokenB' },
-  // ... other stores
+const initialIdleElements = [
+  { name: 'Element 1', TagID: '1100EE00E2000020401302262550CB697740' },
+  { name: 'Element 2', TagID: '1100EE00E2000020401302232560CACF6939' },
+  { name: 'Element 3', TagID: '1100EE00E2000020401302182510C3D16B04' },
+  // Add more elements here...
 ];
 
 const App = () => {
-  const [selectedStores, setSelectedStores] = useState([]);
-  const [selectedElements, setSelectedElements] = useState([]);
-  const [messageSent, setMessageSent] = useState(false);
+  const [stores, setStores] = useState(initialStores);
+  const [idleElements, setIdleElements] = useState(initialIdleElements);
 
-  const handleStoreSelection = (store) => {
-    if (selectedStores.includes(store)) {
-      setSelectedStores(selectedStores.filter(s => s !== store));
-    } else {
-      setSelectedStores([...selectedStores, store]);
-    }
+  const handleAddElementToStore = (element, storeIndex) => {
+    const updatedStores = [...stores];
+    updatedStores[storeIndex].elements.push(element);
+    setIdleElements(idleElements.filter(el => el.TagID !== element.TagID));
+    setStores(updatedStores);
   };
 
-  const handleElementSelection = (element, store) => {
-    setSelectedElements(prevElements => {
-      const updatedElements = prevElements.filter(el => el.store !== store);
-      return [...updatedElements, { ...element, store }];
-    });
+  const handleRemoveElementFromStore = (storeIndex, elementIndex) => {
+    const updatedStores = [...stores];
+    const removedElement = updatedStores[storeIndex].elements.splice(elementIndex, 1)[0];
+    setIdleElements([...idleElements, removedElement]);
+    setStores(updatedStores);
   };
 
-  const handleEmitSignal = async () => {
-    setMessageSent(true);
-    for (const store of selectedStores) {
-      const selectedElementsForStore = selectedElements.filter(el => el.store === store);
-      const requestBody = {
-        ts: Date.now(),
-        Latitude: store.latitude,
-        Longitude: store.longitude,
-        tagsArray: selectedElementsForStore.map((element, index) => ({
-          TagID: element.tagID,
-          Rssi: -59,
-          Numero: index
-        })),
-        BodegaID: store.bodegaID
-      };
+  const handleToggleReady = (storeIndex) => {
+    const updatedStores = [...stores];
+    updatedStores[storeIndex].ready = !updatedStores[storeIndex].ready;
+    setStores(updatedStores);
+  };
 
-      await fetch(`http://api.sighums.com:8080/api/v1/${store.tbToken}/telemetry`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-      });
+  const handleSendMessage = async (store) => {
+    const url = `http://api.sighums.com:8080/api/v1/${store.tbToken}/telemetry`;
+    const message = {
+      ts: Date.now(),
+      Latitude: store.latitude,
+      Longitude: store.longitude,
+      tagsArray: store.elements.map((element, index) => ({
+        TagID: element.TagID,
+        Rssi: -59,
+        Numero: index
+      })),
+      BodegaID: store.BodegaId
+    };
 
-      await new Promise(resolve => setTimeout(resolve, 1000));
-    }
-
-    setMessageSent(false);
-    setSelectedStores([]);
-    setSelectedElements([]);
+    // Simulate sending message
+    console.log('Sending message:', url, message);
+    await new Promise(resolve => setTimeout(resolve, 1000));
   };
 
   return (
-    <div className="app">
-      <div className="stores">
-        <h2>Select Stores:</h2>
-        {stores.map(store => (
-          <label key={store.name}>
+    <div>
+      {stores.map((store, storeIndex) => (
+        <div key={storeIndex}>
+          <h2>{store.name}</h2>
+          <label>
+            Ready:
             <input
               type="checkbox"
-              checked={selectedStores.includes(store)}
-              onChange={() => handleStoreSelection(store)}
+              checked={store.ready}
+              onChange={() => handleToggleReady(storeIndex)}
             />
-            {store.name}
-            {selectedStores.includes(store) && (
-              <div className="store-elements">
-                <h3>Elements in {store.name}:</h3>
-                {initialElements.map(element => (
-                  !selectedElements.find(el => el.tagID === element.tagID && el.store === store) && (
-                    <button
-                      key={element.tagID}
-                      onClick={() => handleElementSelection(element, store)}
-                      disabled={selectedElements.some(el => el.store === store)}
-                    >
-                      Add {element.name}
-                    </button>
-                  )
-                ))}
-              </div>
-            )}
           </label>
-        ))}
+          <ul>
+            {store.elements.map((element, elementIndex) => (
+              <li key={elementIndex}>
+                {element.name} - {element.TagID}
+                <button onClick={() => handleRemoveElementFromStore(storeIndex, elementIndex)}>
+                  Remove
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+      <div>
+        <h2>Idle Elements</h2>
+        <ul>
+          {idleElements.map((element, elementIndex) => (
+            <li key={elementIndex}>
+              {element.name} - {element.TagID}
+              <button onClick={() => handleAddElementToStore(element, 0)}>Add to Store A</button>
+              {/* Add more buttons to select different stores */}
+            </li>
+          ))}
+        </ul>
       </div>
-      <div className="selected-elements">
-        <h2>Selected Elements:</h2>
-        {selectedElements.map(element => (
-          <div key={element.tagID} className="selected-element">
-            <span>{element.name}</span>
-            <span>Store: {element.store.name}</span>
-          </div>
-        ))}
-      </div>
-      <button
-        onClick={handleEmitSignal}
-        disabled={selectedStores.length === 0 || selectedElements.length === 0 || messageSent}
-      >
+      <button onClick={async () => {
+        for (const store of stores) {
+          if (store.ready) {
+            await handleSendMessage(store);
+          }
+        }
+      }}>
         Emitir Señal
       </button>
     </div>
